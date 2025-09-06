c6e7d6deeb09331ae66e82a837d5c93e07a03ad2
lakshganga07 <lakshganga@gmail.com>
2025-09-06 22:43:05 +0530

    Day 02 coding - Variables

---
+++ Day02_Variables.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+def main():
+    title = "Harry Potter"  # string
+    pages = 350  # integer
+    rating = 9.75  # float
+    is_available = True  # boolean
+
+if __name__ == '__main__':
+    main()
