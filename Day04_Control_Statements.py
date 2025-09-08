2e401fad9321355d3df70985ea3ac42ba711ae77
lakshganga07 <lakshganga@gmail.com>
2025-09-08 22:34:39 +0530

    Day 04 coding - Control Statements

---
+++ Day04_Control_Statements.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def main():
+    age = 18
+
+    if age >= 18:
+        print("You can vote ✅")
+    else:
+        print("You are too young ❌")
+
+if __name__ == '__main__':
+    main()
