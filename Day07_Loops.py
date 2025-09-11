93af33343f58cb617d877ceed01d354665c060d6
lakshganga07 <lakshganga@gmail.com>
2025-09-11 22:34:49 +0530

    Day 07 coding - Program to try loops

---
+++ Day07_Loops.py
@@ -0,0 +1,13 @@
+def main():
+   #for loop
+    print('*******************for loop program**************************************************')
+    for i in range(1,11):
+        print("Chocolate :", i)
+    #while loop
+    print('*******************while loop program**************************************************')
+    water_level=0
+    while (water_level<5):
+        print('water_level :' ,water_level)
+        water_level+=1
+if __name__ == '__main__':
+    main()
